517a3ef05817b171df7bb8fb9c37a93bb33c7bbf
Nathan Avilez <tungsteno22@gmail.com>
2018-09-25 16:13:21 -0500

    I hate CSS

--- src/marker.js
+++ src/marker.js
@@ -1,34 +1,24 @@
-const mapboxgl = require('mapbox-gl')
+const mapboxgl = require('mapbox-gl');
 
 const newMarker = (type, location) => {
-    switch(type){
-        case 'hotel':
-        console.log("This is Hotel")
-        const markerDomElHotel = document.createElement("div"); 
-        markerDomElHotel.style.width = "32px";
-        markerDomElHotel.style.height = "39px";
-        markerDomElHotel.style.backgroundImage = "url(http://i.imgur.com/D9574Cu.png)";
-        
-        return new mapboxgl.Marker(markerDomElHotel).setLngLat(location); 
-        // break;
+  const markerDomEl = document.createElement('div');
+  markerDomEl.style.width = '32px';
+  markerDomEl.style.height = '39px';
 
-        case 'restaurant':
-        const markerDomElRest = document.createElement("div");
-        markerDomElRest.style.width = "32px";
-        markerDomElRest.style.height = "39px";
-        markerDomElRest.style.backgroundImage = "url(http://i.imgur.com/cqR6pUI.png)";
-        
-        return new mapboxgl.Marker(markerDomElRest).setLngLat(location); 
-        // break;
+  switch (type) {
+    case 'hotel':
+      markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/D9574Cu.png)';
+      break;
 
-        default:
-        const markerDomElActivity = document.createElement("div"); 
-        markerDomElActivity.style.width = "32px";
-        markerDomElActivity.style.height = "39px";
-        markerDomElActivity.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+    case 'restaurant':
+      markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/cqR6pUI.png)';
 
-        return new mapboxgl.Marker(markerDomElActivity).setLngLat(location); 
-    }
-}
+      break;
 
-module.exports = {newMarker}
+    default:
+      markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+  }
+  return new mapboxgl.Marker(markerDomEl).setLngLat(location);
+};
+
+module.exports = { newMarker };
